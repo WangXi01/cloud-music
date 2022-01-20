1a40583cc19368a1f2a1c16bbd08ac26498797ff
wangxi <wangxi@bop.com>
2022-01-20 10:26:19 +0800

    首页样式

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+// 全局样式
 import { GlobalStyle } from './style';
 import { IconStyle } from './assets/iconfont/iconfont';
+// 路由
+import { renderRoutes } from 'react-router-config'; //renderRoutes 读取路由配置转化为 Route 标签
+import routes from '@/routes/index.js';
+import { HashRouter } from 'react-router-dom';
+// redux
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
